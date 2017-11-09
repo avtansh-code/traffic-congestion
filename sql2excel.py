d634484d1bbdfed073c74317f665b6b23fc12b4e
avtansh-code <avtanshgupta1995@gmail.com>
2017-11-09 23:05:07 +0530

    File to convert sql to excel

---
+++ sql2excel.py
@@ -0,0 +1,29 @@
+import MySQLdb
+import pandas as pd
+
+user = 'root' # your username
+passwd = 'root' # your password
+host = 'localhost' # your host
+db = 'traffic' # database where your table is stored
+table = 'traffic_data' # table you want to save
+
+con = MySQLdb.connect(user=user, passwd=passwd, host=host, db=db)
+cursor = con.cursor()
+
+query = "SELECT * FROM %s;" % table
+cursor.execute(query)
+
+res = cursor.fetchall()
+
+df = pd.DataFrame(data=list(res))
+
+df.columns = ['Location','Curr Speed', 'Norm Speed', 'Date', 'Time', 'Congestion']
+
+print df
+
+writer = pd.ExcelWriter('output.xlsx')
+df.to_excel(writer,'Sheet1')
+writer.save()
+
+print('Done')
+
