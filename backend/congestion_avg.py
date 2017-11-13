5663ac6566977563a2fab2119005bd5292693bd4
kirti22jain <kirti22jain@gmail.com>
2017-11-13 19:59:50 +0530

    updating congestion_avg

--- backend/congestion_avg.py
+++ backend/congestion_avg.py
@@ -6,7 +6,5 @@ import seaborn as sns
 import xlrd
 
 data=pd.read_excel("output.xlsx")
-#data = xlrd.open_workbook("output.xlsx")
-#sheet=data.sheet_by_name('Sheet1')
 congestion_avg=data['Congestion'].mean()
 print(congestion_avg)
