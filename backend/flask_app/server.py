17e330549e95166b36d02aa7bcab65ef8f1f0205
Avtansh Gupta <avtanshgupta1995@gmail.com>
2017-11-29 23:56:44 +0530

    Login error handling server side

--- backend/flask_app/server.py
+++ backend/flask_app/server.py
@@ -71,84 +71,28 @@ def login():
         return jsonify({"msg": "Missing username parameter"}), Status.HTTP_BAD_REQUEST
     if not password:
         return jsonify({"msg": "Missing password parameter"}), Status.HTTP_BAD_REQUEST
-
-    db = MySQLdb.connect(host="localhost",  # your host 
-                        user="root",       # username
-                        passwd="root",     # password
-                        db="traffic")
-    query = "select password from users where username='"+username+"'"
-    cur = db.cursor()
-    cur.execute(query)
-    count = cur.rowcount
-    if count == 0:
-        logger.info("Login Failed Due to missing username")
-        return jsonify({"msg": "Bad username or password"}), Status.HTTP_BAD_UNAUTHORIZED
-    data = cur.fetchall()
-    if password != data[0][0]:
-        logger.info("Login Failed Due to incorrect password")
-        return jsonify({"msg": "Bad username or password"+data}), Status.HTTP_BAD_UNAUTHORIZED
-
-    ret = {'jwt': create_jwt(identity=username), 'exp': datetime.utcnow() + current_app.config['JWT_EXPIRES']}
-    return jsonify(ret), Status.HTTP_OK_BASIC
-
-
-@app.route('/api/protected', methods=['GET'])
-@jwt_required
-def get_data():
-    """Get dummy data returned from the server."""
-    jwt_data = get_jwt()
-    if jwt_data['roles'] != 'admin':
-        return jsonify(msg="Permission denied"), Status.HTTP_BAD_FORBIDDEN
-
-    identity = get_jwt_identity()
-    if not identity:
-        return jsonify({"msg": "Token invalid"}), Status.HTTP_BAD_UNAUTHORIZED
-
-    data = {'Heroes': ['Hero1', 'Hero2', 'Hero3']}
-    json_response = json.dumps(data)
-    return Response(json_response,
-                    status=Status.HTTP_OK_BASIC,
-                    mimetype='application/json')
-
-def json_serial(obj):
-    """JSON serializer for objects not serializable by default json code"""
-
-    if isinstance(obj, (datetime, date)):
-        return obj.isoformat()
-    if isinstance(obj, timedelta):
-	return str(obj)
-    raise TypeError ("Type %s not serializable" % type(obj))
-
-@app.route('/api/getLocationDetails', methods=['POST'])
-@jwt_required
-def post_location_data():
-    logger.info('Getting data')
-    params = request.get_json()
-    logger.info(params)
-    location = params.get('location', None)
     try:
         db = MySQLdb.connect(host="localhost",  # your host 
-                        user="root",           # username
-                        passwd="root",         # password
-                        db="traffic")
+                            user="root",       # username
+                            passwd="root",     # password
+                            db="traffic")
+        query = "select password from users where username='"+username+"'"
         cur = db.cursor()
-        now = datetime.now()
-        currdate = now.strftime("%Y-%m-%d")
-        logger.info(location)
-        query = 'select * from '+ location +' where date="'+ currdate +'"'
         cur.execute(query)
-        rows = cur.fetchall()
-        print rows
-        logger.info('Rows Fetched')
-        json_string = json.dumps(rows, default=json_serial)
-        db.commit()
-        db.close()
-        return Response(json_string,
-                        status=Status.HTTP_OK_BASIC,
-                        mimetype='application/json')
+        count = cur.rowcount
+        if count == 0:
+            logger.info("Login Failed Due to missing username")
+            return jsonify({"msg": "Incorrect Username"}), Status.HTTP_BAD_UNAUTHORIZED
+        data = cur.fetchall()
+        if password != data[0][0]:
+            logger.info("Login Failed Due to incorrect password")
+            return jsonify({"msg": "Incorrect password"}), Status.HTTP_BAD_UNAUTHORIZED
+
+        ret = {'jwt': create_jwt(identity=username), 'exp': datetime.utcnow() + current_app.config['JWT_EXPIRES']}
+        return jsonify(ret), Status.HTTP_OK_BASIC
     except:
         logger.info('Failed')
-        return jsonify({"msg": "Row Not Fetched"}), Status.HTTP_BAD_REQUEST
+        return jsonify({"msg": "Server Error"}), Status.HTTP_BAD_REQUEST
 
 
 def main():
